3ca5f87050d83b43ef19fb96ea6de4c9829c0183
Aleksandr Seymor <godseymor@gmail.com>
2019-05-12 18:22:51 +0500

    Добавляет значения в тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,8 +3,12 @@ import App from './app';
 import renderer from 'react-test-renderer';
 
 it(`renders correctly`, () => {
+  const startButtonClickHandler = () => null;
   const tree = renderer
-    .create(<App/>)
+    .create(<App
+      time={5}
+      errorCount={3}
+      onButtonClick={startButtonClickHandler} />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -3,8 +3,9 @@ import WelcomeScreen from './welcome-screen';
 import renderer from 'react-test-renderer';
 
 it(`rednders correctly`, () => {
+  const onButtonClick = () => null;
   const tree = renderer
-    .create(<WelcomeScreen time={5} errorCount={3} />)
+    .create(<WelcomeScreen time={5} errorCount={3} onButtonClick={onButtonClick} />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
